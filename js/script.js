60344874fe2c1ba3654fb34edc09d912ea1e3ee2
Veetl <80835603+Veetl@users.noreply.github.com>
2021-10-24 20:16:43 +0200

    delivery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,58 @@
+/* Question 1
+
+const dog = {
+    complain: "Meow!",
+    dogMeow: function(){
+        console.log(this.complain);
+    }
+};
+
+dog.dogMeow();
+
+*/
+
+/* Question 2
+
+const heading = document.querySelector("h3");
+
+console.log(heading)
+
+heading.innerHTML = "Updated header";
+
+
+// Question 3
+
+heading.style.fontSize = "2em";
+
+// Question 4
+
+heading.classList.add('subheading');
+
+*/
+
+/* Question 5
+
+const paragraphs = document.querySelectorAll("p");
+
+console.log(paragraphs);
+
+for(let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color = "red"
+}
+
+*/
+
+/* Question 6
+
+const resultsContainer = document.querySelector(".results");
+
+resultsContainer.innerHTML = "<p>New paragraph </p>";
+resultsContainer.style.backgroundColor = "yellow";
+
+*/
+
+/* Question 7
+
 const cats = [
     {
         name: "Blob",
@@ -12,3 +67,16 @@ const cats = [
     }
 ];
 
+function names(list) {
+    for(let i = 0; i < cats.name.length; i++) {
+       console.log(cats.name[i]);
+    }
+
+}
+names();
+
+// Could not make this work...
+*/
+
+// Question 8
+// question 8 is a lot like question 7. I do not understand how to log name from a var/const without displaying the whole array.
